7a559073b4a9abfaf709f0d2ebb988b273ee97ab
ropats16 <rohitcpathare@gmail.com>
2022-05-01 01:48:22 +0530

    2. Chainlink VRF V2 Contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,46 @@
-require("@nomiclabs/hardhat-waffle");
+const { version } = require("chai");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-ethers")
+require("hardhat-deploy");
+require("./tasks");
+require("dotenv").config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const defaultNetwork = "hardhat";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.24",
+      },
+      {
+        version: "0.8.7",
+      }
+    ],
+  },
+  defaultNetwork,
+  //configuring network chain Ids for the deploy script
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    rinkeby: {
+      chainId: 4,
+      url: process.env.RINKEBY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000, 
+      gasPrice: 8000000000,
+    },
+  },
+  // when we get fake accounts, deployer is always at index 0
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
